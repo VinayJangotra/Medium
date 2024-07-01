759742d9b25f7b351cb489ab341dab5078dd9538
VinayJangotra <109384165+VinayJangotra@users.noreply.github.com>
2024-07-01 15:55:16 +0530

    Basic setup and Defining the routes
    In this we define the basic setup for the hono and dd the routes

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import { Hono } from "hono";
+
+const app = new Hono();
+// Routes
+app.post("/api/v1/user/signup", (c) => {
+  return c.text("Hello Hono!");
+});
+app.post("/api/v1/user/signin", (c) => {
+  return c.text("Hello Hono!");
+});
+app.post("/api/v1/user/blog", (c) => {
+  return c.text("Hello Hono!");
+});
+app.put("/api/v1/blog", (c) => {
+  return c.text("Hello Hono!");
+});
+app.get("/api/v1/blog", (c) => {
+  return c.text("Hello Hono!");
+});
+app.post("/api/v1/signup", (c) => {
+  return c.text("Hello Hono!");
+});
+app.get("/api/v1/user/blog/blog", (c) => {
+  return c.text("Hello Hono!");
+});
+export default app;
